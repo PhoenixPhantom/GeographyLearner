c82c2e9ab5f3d310f3b8986a99ed1cbbf710be5f
Raffael Scagnetti <78640383+PhoenixPhantom@users.noreply.github.com>
2024-07-09 22:11:45 +0200

    Package the MacOS build (& actually upload it)

---
+++ packages/GeographyLearnerMacosPackage/packages/main.LearnerAndEditor/meta/installscript.js
@@ -0,0 +1,8 @@
+function Component() {
+}
+
+Component.prototype.createOperations = function()
+{
+    // call default implementation to actually install the GeographyLearner
+    component.createOperations();
+}
